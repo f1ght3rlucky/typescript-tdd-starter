104bd3a3f25f8a8769ff1b9b69da9b5f9c9b66c7
f1ght3rlucky <gromanadzeweb@gmail.com>
2021-08-11 17:11:35 +0200

    first commit

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,5 @@
 describe('test', () => {
   test('add', async () => {
-    expect(12 + 1).toEqual(13);
+    expect(1 + 1).toEqual(2);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('Hello world!');
+export class Index {
+  getUser() {
+    return true;
+  }
+}
